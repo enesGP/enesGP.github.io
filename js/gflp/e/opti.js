7d30e762e814c25982cdd6c9ebf6b33dcfa014d2
pikayutmg <117130976+pikayutmg@users.noreply.github.com>
2023-11-16 20:41:51 +0100

    Update opti.js

--- js/gflp/e/opti.js
+++ js/gflp/e/opti.js
@@ -3,5 +3,5 @@
   // Vérifier si la balise iframe est présente
     if (window.self === window.top) {
       // Rediriger l'utilisateur
-      window.location.href = 'https://pikayutmg.github.io/redirect/beta';
+      window.location.href = 'https://pikayutmg.github.io/redirect/stop';
     }
